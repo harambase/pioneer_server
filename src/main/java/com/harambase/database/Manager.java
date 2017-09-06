ff2256028fdcc2df73c96ff5c843e22bda7fb17e
Shilei Lin <lin.shilei@outlook.com>
2017-09-06 18:14:27 +0800

    修改返回信息

--- src/main/java/com/harambase/database/Manager.java
+++ src/main/java/com/harambase/database/Manager.java
@@ -1,12 +1,35 @@
 package com.harambase.database;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.jdbc.ScriptRunner;
+
+import java.sql.*;
+import java.util.ResourceBundle;
+
 public class Manager {
+
+    private static ResourceBundle resourceBundle = ResourceBundle.getBundle("db");
+    private final static String DRIVER   = resourceBundle.getString("jdbc.driver");
+    private final static String URL      = resourceBundle.getString("jdbc.url");
+    private final static String USERNAME = resourceBundle.getString("jdbc.username");
+    private final static String PASSWORD = resourceBundle.getString("jdbc.password");
+
     public static void init(){
 
     }
 
-    public static void reset(){
+    public static void reset() {
+        try {
+            Class.forName(DRIVER).newInstance();
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            ScriptRunner runner = new ScriptRunner(conn);
+            runner.setErrorLogWriter(null);
+            runner.setLogWriter(null);
+            runner.runScript(Resources.getResourceAsReader("/pioneer.sql"));
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void restore(){
@@ -15,5 +38,9 @@ public class Manager {
 
     public static void backup(){
 
+    }
+
+    public static void main(String[] args){
+        reset();
     }
 }
