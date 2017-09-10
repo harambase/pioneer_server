499a386929e84fbbc17277712bcca56dca8a284a
Shilei Lin <lin.shilei@outlook.com>
2017-09-10 22:26:48 +0800

    新UI

--- src/main/java/com/harambase/pioneer/web/WebController.java
+++ src/main/java/com/harambase/pioneer/web/WebController.java
@@ -10,37 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
     
-    @RequestMapping("/welcomeAdmin")
-    public String welcomeAdmin(){
-        return "welcomeAdmin";
-    }
-    @RequestMapping("/index")
-    public String index(){
-        return "index";
-    }
-    @RequestMapping("/manageCourse")
-    public String adminManageCourse(){
-        return "course/manageCourse";
-    }
-    @RequestMapping("/manageTranscript")
-    public String adminManageTranscript(){
-        return "transcript/manageTranscript";
-    }
-    @RequestMapping("/manageUser")
-    public String adminManageUser(){
-        return "account/manageUser";
-    }
-    @RequestMapping("/manageAdvising")
-    public String adminManageAdvising(){
-        return "advising/manageAdvising";
-    }
-    @RequestMapping("/adminProfile")
-    public String adminProfile(){
-        return "account/adminProfile";
-    }
-    @RequestMapping("/welcome")
-    public String welcome(){
-        return "welcome";
+//    @RequestMapping("/welcomeAdmin")
+//    public String welcomeAdmin(){
+//        return "welcomeAdmin";
+//    }
+   
+    @RequestMapping("/signIn")
+    public String login(){
+        return "src/auth";
     }
     
 }
