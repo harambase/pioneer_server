e20d058a5e766d5e29b4ecf52c633ed086b92987
Shilei Lin <lin.shilei@outlook.com>
2017-09-19 11:12:14 +0800

    将SPRING MVC工程转为 SpringBoot

---
+++ src/main/java/com/harambase/pioneer/web/StaticContorller.java
@@ -0,0 +1,20 @@
+package com.harambase.pioneer.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Controller
+public class StaticContorller extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+
+        registry.addResourceHandler("/styles/**").addResourceLocations("classpath:/static/styles/");
+        registry.addResourceHandler("/scripts/**").addResourceLocations("classpath:/static/scripts/");
+        registry.addResourceHandler("/plugins/**").addResourceLocations("classpath:/static/plugins/");
+        registry.addResourceHandler("/assets/img/**").addResourceLocations("classpath:/static/images/");
+
+        super.addResourceHandlers(registry);
+    }
+}
