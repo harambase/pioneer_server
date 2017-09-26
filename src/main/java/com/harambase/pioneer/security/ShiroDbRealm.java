2403bedac39c3a6833ac3688d5db28463cdb0966
Shilei Lin <lin.shilei@outlook.com>
2017-09-26 17:45:29 +0800

    Shiro权限管理

--- src/main/java/com/harambase/pioneer/security/ShiroDbRealm.java
+++ src/main/java/com/harambase/pioneer/security/ShiroDbRealm.java
@@ -2,6 +2,7 @@ package com.harambase.pioneer.security;
 
 import com.harambase.pioneer.pojo.Person;
 import com.harambase.pioneer.security.service.ShiroService;
+import com.harambase.pioneer.service.Impl.PersonServiceImpl;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authc.credential.CredentialsMatcher;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
@@ -21,58 +22,26 @@ public class ShiroDbRealm extends AuthorizingRealm {
      * 登录认证
      */
     @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken)
-            throws AuthenticationException {
-        UsernamePasswordToken token = (UsernamePasswordToken) authcToken;
-        Person person = shiroService.user(token.getUsername());
-        SimpleAuthenticationInfo info = shiroService.info(person, super.getName());
-        return info;
-    }
-
-//    @Override
-//    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-//        return null;
-//    }
-
-    /**
-     * 权限认证
-     */
-    @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-//        IShiro shiroFactory = ShiroFactroy.me();
-//        ShiroUser shiroUser = (ShiroUser) principals.getPrimaryPrincipal();
-//        List<Integer> roleList = shiroUser.getRoleList();
-//
-//        Set<String> permissionSet = new HashSet<>();
-//        Set<String> roleNameSet = new HashSet<>();
-//
-//        for (Integer roleId : roleList) {
-//            List<String> permissions = shiroFactory.findPermissionsByRoleId(roleId);
-//            if (permissions != null) {
-//                for (String permission : permissions) {
-//                    if (ToolUtil.isNotEmpty(permission)) {
-//                        permissionSet.add(permission);
-//                    }
-//                }
-//            }
-//            String roleName = shiroFactory.findRoleNameByRoleId(roleId);
-//            roleNameSet.add(roleName);
-//        }
-
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+//        logger.debug("登录验证后进行权限认证....");
         SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
-//        info.addStringPermissions(permissionSet);
-//        info.addRoles(roleNameSet);
         return info;
     }
-//
-//    /**
-//     * 设置认证加密方式
-//     */
-//    @Override
-//    public void setCredentialsMatcher(CredentialsMatcher credentialsMatcher) {
-//        HashedCredentialsMatcher md5CredentialsMatcher = new HashedCredentialsMatcher();
-//        md5CredentialsMatcher.setHashAlgorithmName(ShiroKit.hashAlgorithmName);
-//        md5CredentialsMatcher.setHashIterations(ShiroKit.hashIterations);
-//        super.setCredentialsMatcher(md5CredentialsMatcher);
-//    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+//        logger.debug("登录操作进行登录认证......");
+        UsernamePasswordToken  token = (UsernamePasswordToken)authenticationToken;
+
+        Person user = shiroService.user(token.getUsername());
+        if (user == null) {
+            // 没找到帐号
+            throw new UnknownAccountException("没有在本系统中找到对应的用户信息。");
+        }
+        //简单验证
+        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(
+                user.getUsername(),user.getPassword(),getName());
+
+        return info;
+    }
 }
--- src/main/java/com/harambase/pioneer/security/config/ShiroConfig.java
+++ src/main/java/com/harambase/pioneer/security/config/ShiroConfig.java
@@ -1,169 +1,77 @@
 package com.harambase.pioneer.security.config;
 
-import com.harambase.pioneer.security.ShiroDbRealm;
-import com.harambase.pioneer.security.properties.PioneerProperties;
-import org.apache.shiro.cache.CacheManager;
-import org.apache.shiro.codec.Base64;
-import org.apache.shiro.spring.LifecycleBeanPostProcessor;
-import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
-import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
-import org.apache.shiro.web.mgt.CookieRememberMeManager;
-import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-import org.apache.shiro.web.servlet.Cookie;
-import org.apache.shiro.web.servlet.ShiroHttpSession;
-import org.apache.shiro.web.servlet.SimpleCookie;
-import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
-import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
-import org.springframework.beans.factory.config.MethodInvokingFactoryBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
-
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
-/**
- * shiro权限管理的配置
- *
- * @author youe
- * 2016年11月14日 下午3:03:44
- */
+import com.harambase.pioneer.security.ShiroDbRealm;
+import com.harambase.pioneer.security.properties.ShiroSessionListener;
+//import org.apache.shiro.cache.ehcache.EhCacheManager;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.SessionListener;
+import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
-public class ShiroConfig {
+public class ShiroConfig{
 
     /**
-     * 安全管理器
+     * ShiroFilterFactoryBean 处理拦截资源文件问题。
+     * 注意：单独一个ShiroFilterFactoryBean配置是或报错的
+     * 初始化ShiroFilterFactoryBean的时候需要注入：SecurityManager
      */
     @Bean
-    public DefaultWebSecurityManager securityManager(CookieRememberMeManager rememberMeManager, DefaultWebSessionManager defaultWebSessionManager) {
+    public ShiroFilterFactoryBean shirFilter(SecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+
+        // 必须设置 SecurityManager
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+
+        // 拦截器.
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
+
+        //filterChainDefinitionMap.put("/page/*", "authc");
+        // 配置退出过滤器,其中的具体的退出代码Shiro已经替我们实现了
+        //filterChainDefinitionMap.put("/security/logoff", "logout");
+
+        shiroFilterFactoryBean.setLoginUrl("/auth");
+        // 未授权界面;
+        shiroFilterFactoryBean.setUnauthorizedUrl("/403");
+
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+
+        return shiroFilterFactoryBean;
+    }
+
+    @Bean
+    public SecurityManager securityManager() {
         DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-        securityManager.setRealm(this.shiroDbRealm());
-        securityManager.setRememberMeManager(rememberMeManager);
-        securityManager.setSessionManager(defaultWebSessionManager);
+        ShiroDbRealm myRealm = new ShiroDbRealm();
+        securityManager.setRealm(myRealm);
+        securityManager.setSessionManager(sessionManager());
+//        securityManager.setCacheManager(ehCacheManager());
         return securityManager;
     }
 
-    /**
-     * session管理器
-     */
     @Bean
-    public DefaultWebSessionManager defaultWebSessionManager(PioneerProperties pioneerProperties) {
+    public SessionManager sessionManager() {
         DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
-        sessionManager.setSessionValidationInterval(pioneerProperties.getSessionValidationInterval() * 1000);
-        sessionManager.setGlobalSessionTimeout(pioneerProperties.getSessionInvalidateTime() * 1000);
-        sessionManager.setDeleteInvalidSessions(true);
-        sessionManager.setSessionValidationSchedulerEnabled(true);
-        Cookie cookie = new SimpleCookie(ShiroHttpSession.DEFAULT_SESSION_ID_NAME);
-        cookie.setName("shiroCookie");
-        cookie.setHttpOnly(true);
-        sessionManager.setSessionIdCookie(cookie);
+        Collection<SessionListener> listeners = new ArrayList<SessionListener>();
+        listeners.add(new ShiroSessionListener());
+        sessionManager.setSessionListeners(listeners);
         return sessionManager;
     }
 
-
-    /**
-     * 项目自定义的Realm
-     */
-    @Bean
-    public ShiroDbRealm shiroDbRealm() {
-
-        return new ShiroDbRealm();
-    }
-
-    /**
-     * rememberMe管理器, cipherKey生成见{@code Base64Test.java}
-     */
-    @Bean
-    public CookieRememberMeManager rememberMeManager(SimpleCookie rememberMeCookie) {
-        CookieRememberMeManager manager = new CookieRememberMeManager();
-        manager.setCipherKey(Base64.decode("Z3VucwAAAAAAAAAAAAAAAA=="));
-        manager.setCookie(rememberMeCookie);
-        return manager;
-    }
-
-    /**
-     * 记住密码Cookie
-     */
-    @Bean
-    public SimpleCookie rememberMeCookie() {
-        SimpleCookie simpleCookie = new SimpleCookie("rememberMe");
-        simpleCookie.setHttpOnly(true);
-        simpleCookie.setMaxAge(7 * 24 * 60 * 60);//7天
-        return simpleCookie;
-    }
-
-    /**
-     * Shiro的过滤器链
-     */
-    @Bean
-    public ShiroFilterFactoryBean shiroFilter(DefaultWebSecurityManager securityManager) {
-        ShiroFilterFactoryBean shiroFilter = new ShiroFilterFactoryBean();
-        shiroFilter.setSecurityManager(securityManager);
-        /**
-         * 默认的登陆访问url
-         */
-        shiroFilter.setLoginUrl("/auth");
-        /**
-         * 登陆成功后跳转的url
-         */
-        shiroFilter.setSuccessUrl("/welcome");
-        /**
-         * 没有权限跳转的url
-         */
-        shiroFilter.setUnauthorizedUrl("/auth");
-        /**
-         * 配置shiro拦截器链
-         *
-         * anon  不需要认证
-         * authc 需要认证
-         * user  验证通过或RememberMe登录的都可以
-         *
-         */
-        Map<String, String> hashMap = new HashMap<>();
-        hashMap.put("/static/**", "anon");
-        hashMap.put("/auth", "anon");
-        hashMap.put("", "anon");
-        hashMap.put("/auth", "anon");
-//        hashMap.put("/kaptcha", "anon");
-        hashMap.put("/**", "user");
-        shiroFilter.setFilterChainDefinitionMap(hashMap);
-        return shiroFilter;
-    }
-
-    /**
-     * 在方法中 注入 securityManager,进行代理控制
-     */
-    @Bean
-    public MethodInvokingFactoryBean methodInvokingFactoryBean(DefaultWebSecurityManager securityManager) {
-        MethodInvokingFactoryBean bean = new MethodInvokingFactoryBean();
-        bean.setStaticMethod("org.apache.shiro.SecurityUtils.setSecurityManager");
-        bean.setArguments(new Object[]{securityManager});
-        return bean;
-    }
-
-    /**
-     * 保证实现了Shiro内部lifecycle函数的bean执行
-     */
-    @Bean
-    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
-        return new LifecycleBeanPostProcessor();
-    }
-
-    /**
-     * 启用shrio授权注解拦截方式，AOP式方法级权限检查
-     */
-    @Bean
-    @DependsOn(value = "lifecycleBeanPostProcessor") //依赖其他bean的初始化
-    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
-        return new DefaultAdvisorAutoProxyCreator();
-    }
-
-    @Bean
-    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(DefaultWebSecurityManager securityManager) {
-        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor =
-                new AuthorizationAttributeSourceAdvisor();
-        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
-        return authorizationAttributeSourceAdvisor;
-    }
+//    @Bean
+//    public EhCacheManager ehCacheManager() {
+//        EhCacheManager ehCacheManager = new EhCacheManager();
+//        //ehCacheManager.setCacheManagerConfigFile("classpath:encache.xml");
+//        return ehCacheManager;
+//    }
 
 }
