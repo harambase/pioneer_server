13cdf8f2b6e1a974a5bec5d7f339ad954963dd14
Shilei Lin <lin.shilei@outlook.com>
2017-10-05 22:28:27 +0800

    调整page

--- src/main/resources/static/scripts/custom-scripts.js
+++ src/main/resources/static/scripts/custom-scripts.js
@@ -1,9 +1,3 @@
-/*------------------------------------------------------
-    Author : www.webthemez.com
-    License: Commons Attribution 3.0
-    http://creativecommons.org/licenses/by/3.0/
----------------------------------------------------------  */
-
 (function ($) {
     "use strict";
     var mainApp = {
@@ -20,6 +14,7 @@
                     $('div.sidebar-collapse').removeClass('collapse')
                 }
             });
+
         },
 
         initialization: function () {
@@ -36,13 +31,13 @@
 			if($(this).hasClass('closed')){
 				$('.navbar-side').animate({left: '0px'});
 				$(this).removeClass('closed');
-				$('#admin-wrapper').animate({'margin-left' : '260px'});
+				$('#page-wrapper').animate({'margin-left' : '260px'});
 				
 			}
 			else{
 			    $(this).addClass('closed');
 				$('.navbar-side').animate({left: '-260px'});
-				$('#admin-wrapper').animate({'margin-left' : '0px'});
+				$('#page-wrapper').animate({'margin-left' : '0px'});
 			}
 		});
     });
