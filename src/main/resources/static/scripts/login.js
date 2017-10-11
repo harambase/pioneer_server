d397d05d10535a276cb15a7123e66cd9585ce8b0
Shilei Lin <lin.shilei@outlook.com>
2017-10-11 18:14:04 +0800

    UI第二版迭代: 利用模版引擎thymeleaf替换公共部分

--- src/main/resources/static/scripts/login.js
+++ src/main/resources/static/scripts/login.js
@@ -1,26 +1,23 @@
-$(function (){
+$("#btnLogin").click(function(){
     var loginForm = $("#loginForm").validate({});
-    $("#btnLogin").click(function(){
-        if(loginForm.form()) {
-            var userid = $("#userid").val();
-            var password = hex_md5($("#password").val());
-            var person = {
-                userid: userid,
-                password: password
-            };
-            $.ajax({
-                url: basePath + "/admin/login",
-                type: "POST",
-                contentType: "application/json; charset=utf-8",
-                data: JSON.stringify(person),
-                success: function (data) {
-                    if (data.code === 2001)
-                        window.location.href = basePath + "/welcome";
-                    else
-                        Showbo.Msg.alert("登录失败", function () {});
-                }
-            });
-        }
-    });
+    if(loginForm.form()) {
+        var userid = $("#userid").val();
+        var password = hex_md5($("#password").val());
+        var person = {
+            userid: userid,
+            password: password
+        };
+        $.ajax({
+            url: basePath + "/admin/login",
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(person),
+            success: function (data) {
+                if (data.code === 2001)
+                    window.location.href = basePath + "/welcome";
+                else
+                    Showbo.Msg.alert("登录失败", function () {});
+            }
+        });
+    }
 });
-
