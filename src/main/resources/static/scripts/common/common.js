e58973493d47cfc35b716e245fce8ed5cb845a4c
Shilei Lin <lin.shilei@outlook.com>
2017-11-06 16:18:35 +0800

    完善辅导关系的功能。

--- src/main/resources/static/scripts/common/common.js
+++ src/main/resources/static/scripts/common/common.js
@@ -1,58 +1,3 @@
-// $(function(){
-//     var user;
-//     $.ajax({
-//         url: basePath + "/admin/get/current",
-//         type: "GET",
-//         async: false,
-//         success: function (data) {
-//             if (data.code === 2001)
-//                 user = data.data;
-//             else
-//                 Showbo.Msg.alert(data.msg, function () {});
-//         }
-//     });
-//     var baseInfo = $("#editUserForm2");
-//
-//     baseInfo.find("#userid").val(user.userid);
-//     baseInfo.find("#username").val(user.username);
-//     baseInfo.find("#firstname").val(user.firstname);
-//     baseInfo.find("#lastname").val(user.lastname);
-//     baseInfo.find("#birthday").val(user.birthday);
-//     baseInfo.find("#email").val(user.email);
-//     baseInfo.find("#qq").val(user.qq);
-//     baseInfo.find("#weChat").val(user.weChat);
-//     baseInfo.find("#tel").val(user.tel);
-//     baseInfo.find("#dorm").val(user.dorm);
-//     baseInfo.find("#pwd").val(user.password);
-//
-//     $("#confirm").click(function (){
-//         var formdata = {
-//             userid    : $("#userid").val(),
-//             username  : $("#username").val(),
-//             firstname : $("#firstname").val(),
-//             lastname  : $("#lastname").val(),
-//             birthday  : $("#birthday").val(),
-//             email     : $("#email").val(),
-//             weChat    : $("#weChat").val(),
-//             tel       : $("#tel").val(),
-//             dorm      : $("#dorm").val(),
-//             qq        : $("#qq").val(),
-//             password  : hex_md5($("#pwd").val())
-//         };
-//         $.ajax({
-//             url:basePath+"/admin/user/update",
-//             type: "POST",
-//             contentType: "application/json; charset=utf-8",
-//             data: JSON.stringify(formdata),
-//             success: function (data) {
-//                 if (data.code === 2001)
-//                     Showbo.Msg.alert("更新成功!", function () {});
-//                 else
-//                     Showbo.Msg.alert(data.msg, function () {});
-//             }
-//         })
-//     });
-// });
 function removeStuFromCourse(studentid, crn){
     $.ajax({
         url: basePath + "/course/student/remove?studentid="+studentid+"&crn="+crn,
@@ -75,4 +20,12 @@ jQuery.validator.addMethod("isDigits", function (value, element) {
 }, "只能输入0-9数字");
 jQuery.validator.addMethod("checkOpPwd", function (value, element) {
     return this.optional(element) || /^[448aabae4a1dc89b48fdfba10d3e2d3f]+$/.test(hex_md5(value));
-}, "操作密码错误")
+}, "操作密码错误");
+
+$(document).on("show.bs.modal", ".modal", function(){
+    $(this).draggable({
+        handle: ".panel-heading"   // 只能点击头部拖动
+    });
+    $(this).css("overflow", "hidden"); // 防止出现滚动条，出现的话，你会把滚动条一起拖着走的
+});
+
