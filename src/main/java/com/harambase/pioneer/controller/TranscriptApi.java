335b6230a20b2df022d70e2ed082a45b29819284
Shilei Lin <lin.shilei@outlook.com>
2017-11-30 18:02:54 +0800

    SwaggerApi修改

--- src/main/java/com/harambase/pioneer/controller/TranscriptApi.java
+++ src/main/java/com/harambase/pioneer/controller/TranscriptApi.java
@@ -2,49 +2,30 @@ package com.harambase.pioneer.controller;
 
 import com.harambase.common.Tags;
 import com.harambase.pioneer.pojo.Person;
+import com.harambase.pioneer.pojo.Transcript;
 import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
 
-@Api(value = "/user", description = "用户系统管理接口")
+@Api(value = "/transcript", description = "用户系统管理接口")
 public interface TranscriptApi {
 
-    @ApiOperation(value = "新增用户", notes = "创建一个新的用户", response = Map.class, tags = {Tags.PERSON})
+    @ApiOperation(value = "更新成绩单", notes = "权限：管理员，教务", response = Map.class, tags = {Tags.PERSON})
     @ApiResponses(value = {@ApiResponse(code = 200, message = "操作成功", response = Map.class)})
-    ResponseEntity create(@ApiParam(value = "用户", required = true) Person user);
+    ResponseEntity update(@ApiParam(value = "成绩单", required = true) Transcript transcript, HttpSession session);
 
-    @ApiOperation(value = "删除一个用户", notes = "删除一个用户", response = Map.class, tags = {Tags.PERSON})
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "操作成功", response = Map.class)})
-    ResponseEntity delete(@ApiParam(value = "用户ID", required = true) String userId);
-
-    @ApiOperation(value = "更新用户", notes = "更新一个用户", response = Map.class, tags = {Tags.PERSON})
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "操作成功", response = Map.class)})
-    ResponseEntity update(@ApiParam(value = "用户", required = true) Person user);
-
-    @ApiOperation(value = "获取用户信息", notes = "获取一个用户信息", response = Map.class, tags = {Tags.PERSON})
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "操作成功", response = Map.class)})
-    ResponseEntity get(@ApiParam(value = "用户ID", required = true) String userId);
-
-    @ApiOperation(value = "获取当前登录的用户信息", notes = "获取一个用户信息", response = Map.class, tags = {Tags.PERSON})
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "操作成功", response = Map.class)})
-    ResponseEntity getCurrentUser(HttpSession session);
-
-    @ApiOperation(value = "用户列表", notes = "only登录用户", response = Map.class, tags = {Tags.PERSON})
+    @ApiOperation(value = "成绩单列表", notes = "权限：管理员，教务，教师，学生", response = Map.class, tags = {Tags.PERSON})
     ResponseEntity list(@ApiParam(value = "start") Integer start,
                         @ApiParam(value = "length") Integer length,
                         @ApiParam(value = "draw") Integer draw,
                         @ApiParam(value = "搜索关键字") String search,
                         @ApiParam(value = "排序方式") String order,
                         @ApiParam(value = "排序列") String orderCol,
-                        @ApiParam(value = "用户类型") String type,
-                        @ApiParam(value = "用户状态") String status);
+                        @ApiParam(value = "学生ID") String studentId,
+                        @ApiParam(value = "课程CRN") String crn);
 
 
-    @ApiOperation(value = "用户列表", notes = "only登录用户", response = Map.class, tags = {Tags.PERSON})
-    ResponseEntity searcUser(@ApiParam(value = "search") String search,
-                             @ApiParam(value = "type") String type,
-                             @ApiParam(value = "status") String status);
 
 }
--- src/main/java/com/harambase/pioneer/controller/TranscriptController.java
+++ src/main/java/com/harambase/pioneer/controller/TranscriptController.java
@@ -9,7 +9,6 @@ import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
@@ -20,7 +19,7 @@ import java.util.Map;
 @RestController
 @CrossOrigin
 @RequestMapping(value = "/transcript")
-public class TranscriptController {
+public class TranscriptController implements TranscriptApi {
     
     private final TranscriptService transcriptService;
     
@@ -29,25 +28,26 @@ public class TranscriptController {
         this.transcriptService = transcriptService;
     }
 
-
+    @Override
     @RequiresPermissions({"admin", "teach"})
     @RequestMapping(produces = "application/json", method = RequestMethod.PUT)
-    public ResponseEntity updateTranscript(@RequestBody Transcript transcript, HttpSession session) {
+    public ResponseEntity update(@RequestBody Transcript transcript, HttpSession session) {
         transcript.setOperator(((Person) session.getAttribute("user")).getUserid());
         HaramMessage haramMessage = transcriptService.updateGrade(transcript);
         return new ResponseEntity<>(haramMessage, HttpStatus.OK);
     }
 
-    @RequiresPermissions({"admin", "teach", "advisor"})
+    @Override
+    @RequiresPermissions({"admin", "teach", "student", "faculty"})
     @RequestMapping(value = {"/{studentId}/course","/{crn}/student"}, produces = "application/json", method = RequestMethod.GET)
-    public ResponseEntity studentCourseList(@RequestParam(value = "start") Integer start,
-                                            @RequestParam(value = "length") Integer length,
-                                            @RequestParam(value = "draw") Integer draw,
-                                            @RequestParam(value = "search[value]") String search,
-                                            @RequestParam(value = "order[0][dir]") String order,
-                                            @RequestParam(value = "order[0][column]") String orderCol,
-                                            @PathVariable(value = "studentId") String studentId,
-                                            @PathVariable(value = "crn") String crn) {
+    public ResponseEntity list(@RequestParam(value = "start") Integer start,
+                               @RequestParam(value = "length") Integer length,
+                               @RequestParam(value = "draw") Integer draw,
+                               @RequestParam(value = "search[value]") String search,
+                               @RequestParam(value = "order[0][dir]") String order,
+                               @RequestParam(value = "order[0][column]") String orderCol,
+                               @PathVariable(value = "studentId") String studentId,
+                               @PathVariable(value = "crn") String crn) {
         Map<String, Object> map = new HashMap<>();
         try {
             HaramMessage message = transcriptService.transcriptList(String.valueOf(start / length + 1), String.valueOf(length), search, order, orderCol, studentId, crn);
