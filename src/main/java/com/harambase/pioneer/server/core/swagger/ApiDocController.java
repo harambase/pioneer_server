4e076b6fe88275ddc0f0a482b2731da7f7c6b424
Shilei Lin <lin.shilei@outlook.com>
2018-01-11 18:14:42 +0800

    设置接口访问权限

--- src/main/java/com/harambase/pioneer/server/core/swagger/ApiDocController.java
+++ src/main/java/com/harambase/pioneer/server/core/swagger/ApiDocController.java
@@ -1,15 +1,31 @@
 package com.harambase.pioneer.server.core.swagger;
 
+import com.harambase.pioneer.server.core.dao.repository.PersonRepository;
+import com.harambase.pioneer.server.core.pojo.base.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Home redirection to swagger api documentation
  */
 @Controller
 public class ApiDocController {
+
+    private final PersonRepository personRepository;
+
+    @Autowired
+    public ApiDocController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
     @RequestMapping(value = "/")
-    public String apiDoc() {
-        return "redirect:swagger-ui.html";
+    public String apiDoc(@RequestParam String userId) {
+        Person person = personRepository.findOne(userId);
+        if (person.getRoleId().contains("1"))
+            return "redirect:swagger-ui.html";
+        else
+            return "error";
     }
 }
