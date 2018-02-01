052f65ef86cc57dbaa64c55a2aef1f224315a70b
Shilei Lin <lin.shilei@outlook.com>
2018-02-01 12:52:12 +0800

    单例模式开发

--- src/main/java/com/harambase/pioneer/server/PioneerApplication.java
+++ src/main/java/com/harambase/pioneer/server/PioneerApplication.java
@@ -1,4 +1,4 @@
-package com.harambase.pioneer.server.application;
+package com.harambase.pioneer.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
